69018282affb41689ae3261ce5943304ac460c11
kkronggiscool <crongie129@gmail.com>
2024-12-20 18:52:51 -0500

    aaa

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'ejs'))
+app.use(express.static('public'));
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.render("index");
+});
+
+app.listen(1000, () => {
+    console.log("p http://localhost:1000 is running on skibidi dop dop")
+});
