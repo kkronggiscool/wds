a8f0ea50123e4ba61d23f9ddb1516ce03fe16897
kkronggiscool <crongie129@gmail.com>
2024-12-20 21:28:35 -0500

    fix express.static for vercel

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const path = require("path");
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'ejs'))
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, '/public/')));
 app.use(express.json());
 
 app.get('/', (req, res) => {
